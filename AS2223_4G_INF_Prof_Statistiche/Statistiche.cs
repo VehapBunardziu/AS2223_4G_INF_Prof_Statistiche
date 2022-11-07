4917db7af446329ec7acfca8606e4085fd1440ba
VehapBunardziu <vehap.bunardziu@studenti.ittsrimini.edu.it>
2022-11-07 10:28:09 +0100

    Aggiungere i file di progetto.

---
+++ AS2223_4G_INF_Prof_Statistiche/Statistiche.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2223_4G_INF_Prof_Statistiche
+{
+    public class Statistiche
+    {
+
+
+        public Statistiche()
+        {
+
+        }
+
+        /// <summary>
+        /// Aggiungi un valore senza peso per le statistiche (media aritmetica)
+        /// </summary>
+        /// <param name="valore"></param>
+        public void AddValue(int valore)
+        { 
+
+        }
+
+        /// <summary>
+        /// Aggiungi un valore con peso per le statistiche (media ponderata)
+        /// </summary>
+        /// <param name="valore"></param>
+        /// <param name="peso"></param>
+        public void AddValue(int valore, double peso)
+        {
+
+        }
+
+        /// <summary>
+        /// Ritorna la media aritmetica dei valori :
+        ///     mediaAritmetica = sommaValori/numeroValori
+        /// </summary>
+        /// <returns></returns>
+        public double MediaAritmetica()
+        {
+
+        }
+
+        /// <summary>
+        /// Ritorna la media ponderata dei valori :
+        ///     mediaPonderata = sommaValoriPerPeso/sommaPesi
+        /// </summary>
+        /// <returns></returns>
+        public double MediaPonderata()
+        {
+
+        }
+
+        /// <summary>
+        /// Azzera i valori inseriti per la statistica
+        /// </summary>
+        public void ClearValue()
+        {
+
+        }
+
+        /// <summary>
+        /// Ritorna il numero di elementi inseriti per la statistica
+        /// </summary>
+        /// <returns></returns>
+        public int CountValue()
+        {
+
+        }
+
+    }
+}
